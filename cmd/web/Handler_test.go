cfa743c484dce6e107f04b564692e2d8f3756728
blakehulett7 <blake.hulett7@gmail.com>
2025-08-01 12:27:41 -0500

    added positive path test case for login handler

--- cmd/web/Handler_test.go
+++ cmd/web/Handler_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
 )
@@ -67,6 +68,49 @@ func Test_app_Home(t *testing.T) {
 		body, _ := io.ReadAll(w.Body)
 		if !strings.Contains(string(body), test.ExpectedHTML) {
 			t.Errorf("%s failed: not getting back expected %s", test.Name, test.ExpectedHTML)
+		}
+	}
+}
+
+func Test_app_Login(t *testing.T) {
+	tests := []struct {
+		Name               string
+		PostedData         url.Values
+		ExpectedStatusCode int
+		ExpectedLocation   string
+	}{
+		{
+			Name: "valid login",
+			PostedData: url.Values{
+				"email":    {"admin@example.com"},
+				"password": {"secret"},
+			},
+			ExpectedStatusCode: http.StatusSeeOther,
+			ExpectedLocation:   "/user/profile",
+		},
+	}
+
+	for _, test := range tests {
+		req, _ := http.NewRequest("POST", "/login", strings.NewReader(test.PostedData.Encode()))
+		req = AddContextAndSessionToRequest(req, app)
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+		w := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(app.Login)
+		handler.ServeHTTP(w, req)
+
+		if w.Code != test.ExpectedStatusCode {
+			t.Errorf("%s failed: Expected status code %d, but got %d\n", test.Name, test.ExpectedStatusCode, w.Code)
+		}
+
+		location, err := w.Result().Location()
+		if err != nil {
+			t.Errorf("%s failed: Error accessing the location returned from the handler %v\n", test.Name, err)
+		}
+
+		if location.String() != test.ExpectedLocation {
+			t.Errorf("%s failed: Expected location %s, but got %s\n", test.Name, test.ExpectedLocation, location.String())
 		}
 	}
 }
--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"log"
 	"os"
 	"testing"
+
+	"github.com/blakehulett7/mctestface-webapp/pkg/db"
 )
 
 var app State
 
 func TestMain(m *testing.M) {
 	app.Session = GetSession()
+	app.DSN = "host=localhost port=5433 user=postgres password=postgres dbname=users sslmode=disable timezone=UTC connect_timeout=5"
+
+	conn, err := app.ConnectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	app.DB = db.PostgresConn{DB: conn}
 
 	os.Exit(m.Run())
 }
