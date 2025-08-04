af6b5ec21f3d710830e6c21344dee5349fdcc4a2
blakehulett7 <blake.hulett7@gmail.com>
2025-08-04 16:32:49 -0500

    i have never like dependencies

--- pkg/repository/dbrepo/Users_postgres_test.go
+++ pkg/repository/dbrepo/Users_postgres_test.go
@@ -2,6 +2,8 @@ package dbrepo
 
 import (
 	"database/sql"
+	"fmt"
+	"log"
 	"os"
 	"testing"
 
@@ -9,6 +11,7 @@ import (
 	_ "github.com/jackc/pgx/v4"
 	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
 )
 
 var (
@@ -25,7 +28,49 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	code := m.Run()
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("Docker not running: %s\n", err)
+	}
 
+	pool = p
+	options := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "14.5",
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		ExposedPorts: []string{"5432"},
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	resource, err := pool.RunWithOptions(&options)
+	if err != nil {
+		pool.Purge(resource)
+		log.Fatalf("Could not start resource: %s\n", err)
+	}
+
+	err = pool.Retry(func() error {
+		var innerErr error
+		testDB, innerErr = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			log.Println("Error:", innerErr)
+			return innerErr
+		}
+
+		return testDB.Ping()
+	})
+	if err != nil {
+		pool.Purge(resource)
+		log.Fatalf("Could not connect to db: %s\n", err)
+	}
+
+	code := m.Run()
 	os.Exit(code)
 }
