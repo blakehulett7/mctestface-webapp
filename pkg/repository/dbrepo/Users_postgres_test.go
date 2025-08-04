373753597087d2585fed67079513f9ffc041effd
blakehulett7 <blake.hulett7@gmail.com>
2025-08-04 16:58:04 -0500

    lol exact wording is required

--- pkg/repository/dbrepo/Users_postgres_test.go
+++ pkg/repository/dbrepo/Users_postgres_test.go
@@ -20,7 +20,7 @@ var (
 	password = "postgres"
 	dbName   = "users_test"
 	port     = "5435"
-	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disabled timezone=UTC connect_timeout=5"
+	dsn      = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
 )
 
 var resource *dockertest.Resource
@@ -28,6 +28,7 @@ var pool *dockertest.Pool
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	log.Println("Sancta Maria...")
 	p, err := dockertest.NewPool("")
 	if err != nil {
 		log.Fatalf("Docker not running: %s\n", err)
@@ -59,7 +60,7 @@ func TestMain(m *testing.M) {
 	err = pool.Retry(func() error {
 		var innerErr error
 		testDB, innerErr = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
-		if err != nil {
+		if innerErr != nil {
 			log.Println("Error:", innerErr)
 			return innerErr
 		}
@@ -71,6 +72,37 @@ func TestMain(m *testing.M) {
 		log.Fatalf("Could not connect to db: %s\n", err)
 	}
 
+	err = CreateTables()
+	if err != nil {
+		log.Fatalf("Could not populate db: %s\n", err)
+	}
+
 	code := m.Run()
+
+	err = pool.Purge(resource)
+	if err != nil {
+		log.Fatalf("could not purge resouces: %s\n", err)
+	}
 	os.Exit(code)
+}
+
+func CreateTables() error {
+	tableSql, err := os.ReadFile("./testdata/users.sql")
+	if err != nil {
+		return err
+	}
+
+	_, err = testDB.Exec(string(tableSql))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestPingDb(t *testing.T) {
+	err := testDB.Ping()
+	if err != nil {
+		t.Error("can't ping db...")
+	}
 }
