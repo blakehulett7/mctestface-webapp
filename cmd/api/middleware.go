0e4a2040d60f5bf3e3f1174675acbf725f233cc8
blakehulett7 <blake.hulett7@gmail.com>
2025-08-07 11:25:59 -0500

    not sure if this will work

---
+++ cmd/api/middleware.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *Bridge) EnableCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8090")
+		if r.Method == "OPTIONS" {
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, X-CSRF-Token, Authorization")
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
+}
